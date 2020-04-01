096f8301fa08d9f2b85f894e644f565eed04e66d
ColinGreeley <37784147+ColinGreeley@users.noreply.github.com>
2020-04-01 10:03:10 -0700

    Add files via upload

---
+++ Deep_Dream.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3.5
+# coding: utf-8
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+from PIL import Image
+import PIL
+#from IPython.display import clear_output, Image, display, HTML
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+model_fn = 'models/tensorflow_inception_graph.pb'
+
+# creating TensorFlow session and loading the model
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with tf.gfile.FastGFile(model_fn, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+imagenet_mean = 117.0
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input':t_preprocessed})
+
+
+# Picking some internal layer. Note that we use outputs before applying the ReLU nonlinearity
+# to have non-zero gradients for features with negative initial activations.
+layer = 'mixed4d_3x3_bottleneck_pre_relu'
+channel = 139 # picking some feature channel to visualize
+
+# start with a gray image with a little noise
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+def showarray(a, fmt='jpeg'):
+    a = np.uint8(np.clip(a, 0, 1)*255)
+    f = BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    #display(Image(data=f.getvalue()))
+    im = Image.open(f)
+    im.show()
+    return im
+    
+def visstd(a, s=0.1):
+    '''Normalize the image range for visualization'''
+    return (a-a.mean())/max(a.std(), 1e-4)*s + 0.5
+
+def T(layer):
+    '''Helper for getting layer output tensor'''
+    return graph.get_tensor_by_name("import/%s:0"%layer)
+
+def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+    
+    img = img0.copy()
+    for i in range(iter_n):
+        g, score = sess.run([t_grad, t_score], {t_input:img})
+        # normalizing the gradient, so the same step size should work 
+        g /= g.std()+1e-8         # for different layers and networks
+        img += g*step
+        print(score, end = ' ')
+    #clear_output()
+    showarray(visstd(img))
+
+
+def tffunc(*argtypes):
+    '''Helper that transforms TF-graph generating function into a regular one.
+    See "resize" function below.
+    '''
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+        return wrapper
+    return wrap
+
+# Helper function that uses TF to resize an image
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+resize = tffunc(np.float32, np.int32)(resize)
+
+
+def calc_grad_tiled(img, t_grad, tile_size=512):
+    '''Compute the value of tensor t_grad over the image in a tiled way.
+    Random shifts are applied to the image to blur tile boundaries over 
+    multiple iterations.'''
+    sz = tile_size
+    h, w = img.shape[:2]
+    sx, sy = np.random.randint(sz, size=2)
+    img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+    grad = np.zeros_like(img)
+    for y in range(0, max(h-sz//2, sz),sz):
+        for x in range(0, max(w-sz//2, sz),sz):
+            sub = img_shift[y:y+sz,x:x+sz]
+            g = sess.run(t_grad, {t_input:sub})
+            grad[y:y+sz,x:x+sz] = g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0)
+
+
+
+
+
+########################################### Display filters  ########################################################
+
+k = np.float32([1,4,6,4,1])
+k = np.outer(k, k)
+k5x5 = k[:,:,None,None]/k.sum()*np.eye(3, dtype=np.float32)
+
+def lap_split(img):
+    '''Split the image into lo and hi frequency components'''
+    with tf.name_scope('split'):
+        lo = tf.nn.conv2d(img, k5x5, [1,2,2,1], 'SAME')
+        lo2 = tf.nn.conv2d_transpose(lo, k5x5*4, tf.shape(img), [1,2,2,1])
+        hi = img-lo2
+    return lo, hi
+
+def lap_split_n(img, n):
+    '''Build Laplacian pyramid with n splits'''
+    levels = []
+    for i in range(n):
+        img, hi = lap_split(img)
+        levels.append(hi)
+    levels.append(img)
+    return levels[::-1]
+
+def lap_merge(levels):
+    '''Merge Laplacian pyramid'''
+    img = levels[0]
+    for hi in levels[1:]:
+        with tf.name_scope('merge'):
+            img = tf.nn.conv2d_transpose(img, k5x5*4, tf.shape(hi), [1,2,2,1]) + hi
+    return img
+
+def normalize_std(img, eps=1e-10):
+    '''Normalize image by making its standard deviation = 1.0'''
+    with tf.name_scope('normalize'):
+        std = tf.sqrt(tf.reduce_mean(tf.square(img)))
+        return img/tf.maximum(std, eps)
+
+def lap_normalize(img, scale_n=4):
+    '''Perform the Laplacian pyramid normalization.'''
+    img = tf.expand_dims(img,0)
+    tlevels = lap_split_n(img, scale_n)
+    tlevels = list(map(normalize_std, tlevels))
+    out = lap_merge(tlevels)
+    return out[0,:,:,:]
+
+def render_lapnorm(t_obj, img0=img_noise, visfunc=visstd,
+                   iter_n=10, step=1.0, octave_n=3, octave_scale=1.4, lap_n=4, id=0):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+    # build the laplacian normalization graph
+    lap_norm_func = tffunc(np.float32)(partial(lap_normalize, scale_n=lap_n))
+
+    img = img0.copy()
+    for octave in range(octave_n):
+        if octave>0:
+            hw = np.float32(img.shape[:2])*octave_scale
+            img = resize(img, np.int32(hw))
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            g = lap_norm_func(g)
+            img += g*step
+            print('.', end = ' ')
+        #clear_output()
+    im = showarray(visfunc(img))
+    im.save('/home/colin/python_scripts/deep_dream/convnet_filters/filter_' + str(id) + '.jpg')
+
+####################################################################################################################
+
+
+
+
+##############################
+#### deep dream algorithm ####
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.5, octave_n=4, octave_scale=1.4, image_name='result'):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+    # split the image into a number of octaves
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+    
+    # generate details octave by octave
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+        #clear_output()
+        im = showarray(img/255.0)
+    im.save('./' + image_name + '.jpg')
+    
+
+#### display settings ####
+##########################
+
+# display filters #
+"""
+for i in range(1,1000):
+    render_lapnorm(T(layer)[:,:,:,i], id=i)
+"""
+#render_lapnorm(T(layer)[:,:,:,3]+T(layer)[:,:,:,20])
+
+# render images #
+
+#img0 = PIL.Image.open('/home/colin/Pictures/landscape.jpg')
+
+#'''
+print('\n\n\n                       ** Welcome to the image trippifier **')
+print('\n          ** Enter an input image, name it, choose the intensity and filter **')
+print('** For better results, run the program again on your output image with a different filter **')
+print('\n\n\nEnter file path for input image.  \nE.g. C:\Pictures\something.jpg')
+img0 = input(':')
+while not os.path.isfile(img0):
+    print('\n', img0, 'does not exist.')
+    img0 = input(':')
+img0 = PIL.Image.open(img0)
+img0 = np.float32(img0)
+#showarray(img0/255.)
+print('\nImage succesfully loaded.\n')
+
+print('\nEnter a name for the result image.')
+image_name = input(':')
+
+print('\nEnter the number of iterations (filter intensity, default=10)')
+iter_number = int(input(':'))
+
+print("\nEnter a filter number (between 0 and 144)")
+filt = 0
+while filt < 1 or filt > 143:
+    filt = int(input(':'))
+
+
+render_deepdream(T(layer)[:,:,:,filt], img0, iter_n=iter_number, image_name=image_name)
+#'''
+
+#render_deepdream(tf.square(T('mixed4c')), img0) # animals(wolves/foxes?)
+#render_deepdream(T(layer)[:,:,:,139], img0) # flowers
+#render_deepdream(T(layer)[:,:,:,11], img0) # clouds
+
+#render_deepdream(T(layer)[:,:,:,20], img0, iter_n=5)
+#render_deepdream(T(layer)[:,:,:,3]+T(layer)[:,:,:,20], img0, iter_n=8)
+#render_deepdream(T(layer)[:,:,:,139], img0, iter_n=15)
